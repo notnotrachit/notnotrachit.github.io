922a79f1fb28b3e66f31aa76418a1fdea7a358b5
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-05-26 22:30:01 +0530

    project page done for desktop

--- components/project.js
+++ components/project.js
@@ -7,20 +7,20 @@ export default function ProjectCard(props) {
     }
 
   return (
-    <div className="card w-96 bg-neutral shadow-xl" data-aos="fade-up">
+    <div className="card w-96 bg-neutral shadow-xl border border-primary" data-aos="fade-up">
       <figure>
         <Image
           src={props.image_url}
           alt="random"
           width={300}
           height={200}
-          className="mt-8 rounded-xl"
+          className="mt-8 rounded-xl border border-primary/50"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{props.name}</h2>
         <p>{props.description}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions justify-start">
           {props.techs &&
             props.techs.map((tech) => (
               <div className="badge badge-outline" key={tech}>
@@ -34,7 +34,7 @@ export default function ProjectCard(props) {
         </label>
       </div>
       <input type="checkbox" id={props.name} className="modal-toggle" />
-      <div className="modal w-[98%] lg:w-full">
+      <div className="modal w-[98%] lg:w-full transition-all ease-in-out">
         <div className="relative border border-primary bg-neutral p-1 lg:p-5 rounded-xl w-full lg:w-1/2">
           <label
             htmlFor={props.name}
@@ -42,7 +42,8 @@ export default function ProjectCard(props) {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">{props.name}</h3>
+          <h3 className="text-3xl font-bold text-center">{props.name}</h3>
+          { props.allow_embed ? (
           <label className="swap text-6xl" id={props.name+"swap"}>
             <div className="flex justify-center group swap-off">
               <div className="grid grid-cols-1 grid-rows-1">
@@ -52,15 +53,6 @@ export default function ProjectCard(props) {
                   width={1000}
                   height={800}
                   className="place-self-center lg:w-5/6 p-2 group-hover:blur-sm transition-all ease-in-out rounded-xl row-start-1 row-end-1 col-start-1 col-end-1 z-[5] group-hover:z-[2]"
-                  style={{
-                    '-ms-zoom': 0.75,
-                    '-moz-transform': 0.75,
-                    '-moz-transform-origin': 0,
-                    '-o-transform': 0.75,
-                    '-o-transform-origin': 0,
-                    '-webkit-transform': 0.75,
-                    '-webkit-transform-origin': 0,
-                  }}
                 />
                 <div className="grid content-center justify-center row-start-1 row-end-1 col-start-1 col-end-1 h-full z-[4]">
                   <button className="btn btn-primary bg-neutral/30 backdrop-blur-xl text-primary hover:text-neutral" onClick={() => swap_live(props.name)}>
@@ -78,21 +70,49 @@ export default function ProjectCard(props) {
               ></iframe>
               <div className="flex w-full my-2">
                 <button className="justify-self-start my-1 btn btn-primary bg-neutral/30 backdrop-blur-xl text-primary hover:text-neutral" onClick={() => swap_live(props.name)}>Back</button>
-                <div className="justify-self-center text-sm px-8 text-center">
-                The preview might look a bit weird inside the iFrame, but it should look fine when you open the project in a new tab.
+                <div className="justify-self-center text-sm px-8 text-center underline underline-offset-2">
+                The preview might look a bit weird inside the iframe, but it should look fine when you open the project in a new tab.
               </div>
-              <a className="justify-self-end my-1 btn btn-primary bg-neutral/30 backdrop-blur-xl text-primary hover:text-neutral" href={props.live_url}>Preview</a>
+              <a className="justify-self-end my-1 btn btn-primary bg-neutral/30 backdrop-blur-xl text-primary hover:text-neutral" href={props.live_url} target="_blank">Preview</a>
               </div>
 
             </div>
           </label>
+          ) :
+            (
+                <div className="flex justify-center">
+                  <Image
+                    src={props.image_url}
+                    alt="project_image"
+                    width={1000}
+                    height={800}
+                    className="place-self-center lg:w-5/6 p-2 group-hover:blur-sm transition-all ease-in-out rounded-xl row-start-1 row-end-1 col-start-1 col-end-1 z-[5] group-hover:z-[2]"
+                  />
+
+              </div>
+            )
+
+          
+          }
+
           <p className="pt-16 pb-4">{props.long_description}</p>
+          <p className="font-bold text-lg">Tech Stack:</p>
           {props.techs &&
             props.techs.map((tech) => (
               <div className="badge badge-outline mx-1" key={tech}>
                 {tech}
               </div>
             ))}
+            <div className="flex justify-center">
+            <div className="btn-group btn-group-vertical lg:btn-group-horizontal">
+                { props.github_url && (
+                <a className="btn btn-sm btn-outline" href={props.github_url} target="_blank">GitHub</a>
+                )}
+                { props.live_url && (
+                <a className="btn btn-sm btn-outline" href={props.live_url} target="_blank">Preview</a>
+                )}
+                </div>
+            </div>
         </div>
       </div>
     </div>
