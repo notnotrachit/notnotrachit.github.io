df9109139d9e4844a3606b453e14635644da40f5
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-09-27 12:34:31 +0530

    Fixed mobile view draggable

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -3,8 +3,7 @@ import TypeIt from "typeit";
 import { useEffect } from "react";
 import Image from "next/image";
 import Draggable, { DraggableCore } from "react-draggable";
-import Github_Graph from "@/components/gitgraph";
-import Link from "next/link";
+import HomeCard from "@/components/home";
 
 export default function Home() {
   useEffect(() => {
@@ -120,8 +119,40 @@ export default function Home() {
 
   return (
     <main className="bg-base-100 overflow-x-clip">
-      <div className="flex justify-center py-5 bg-base-100" data-aos="zoom-in">
-        <Draggable handle="handle">
+      <div className="flex justify-start py-5 bg-base-100" data-aos="zoom-in">
+        <div className="hidden md:flex w-full justify-center">
+          <Draggable handle="handle">
+            <div
+              className="absolute bg-neutral pb-5 text-neutral-content rounded-box text-xl w-11/12 mx-2 lg:w-1/2 border-primary/50 border"
+              id="window"
+            >
+              <handle>
+                <div
+                  className="w-full bg-primary/50 rounded-t-box cursor-move flex justify-end px-2 py-1"
+                  id="windowheader"
+                >
+                  <div className="avatar placeholder mx-1">
+                    <div className="bg-neutral-focus text-neutral-content rounded-full w-3">
+                      <span className="text-xs"></span>
+                    </div>
+                  </div>
+                  <div className="avatar placeholder mx-1">
+                    <div className="bg-warning text-neutral-content rounded-full w-3">
+                      <span className="text-xs"></span>
+                    </div>
+                  </div>
+                  <div className="avatar placeholder mx-1">
+                    <div className="bg-error text-neutral-content rounded-full w-3">
+                      <span className="text-xs"></span>
+                    </div>
+                  </div>
+                </div>
+              </handle>
+              <HomeCard />
+            </div>
+          </Draggable>
+        </div>
+        <div className="md:hidden flex justify-center w-full">
           <div
             className="absolute bg-neutral pb-5 text-neutral-content rounded-box text-xl w-11/12 mx-2 lg:w-1/2 border-primary/50 border"
             id="window"
@@ -148,66 +179,9 @@ export default function Home() {
                 </div>
               </div>
             </handle>
-            <pre data-prefix="$" className="text-success ml-3">
-              <code>$ python about_me.py</code>
-            </pre>
-            <div className="lg:flex lg:px-8">
-              <div className="flex justify-center lg:py-5" data-aos="zoom-in">
-                <div className="avatar hover:scale-110 transition-all ease-in-out">
-                  <div className="w-24 lg:w-48 rounded-full hover:rounded-xl border-2 border-primary hover:border-none hover:ring ring-primary ring-offset-base-100 ring-offset-4 transition-all ease-in-out">
-                    <Image
-                      src="/profile.jpg"
-                      alt="Rachit Khurana"
-                      width={150}
-                      height={150}
-                      className=""
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="w-full h-full m-auto">
-                <div className="flex w-full justify-center" data-aos="zoom-in">
-                  <h1 className="text-2xl lg:text-6xl font-bold mx-12">
-                    Rachit Khurana
-                  </h1>
-                </div>
-                <div className="flex w-full justify-center" data-aos="zoom-in">
-                  <p id="element" className="text-xl lg:text-2xl italic"></p>
-                </div>
-              </div>
-            </div>
-            <p className="ml-5 text-base lg:text-lg leading-tight">
-              a.k.a DiluteWater & notnotrachit, 
-              a passionate Full Stack Developer from India 🇮🇳
-              <br />
-              Open source enthusiast, Pythonista, Linux lover
-              <br />
-              Ex Intern @ Clearmind AI
-              <br />
-              Microsoft Learn Student Ambassador,  Postman Student Expert
-              <br />
-              Chief of Tech @{" "}
-              <a
-                href="https://www.csi-bu.live/"
-                target="_blank"
-                className="underline"
-              >
-                CSI BU
-              </a>
-            </p>
-            <pre data-prefix="$" className="text-success ml-3 mt-5">
-              <code>$ python contributions.py</code>
-            </pre>
-            <Github_Graph />
-            <div className="text-base text-center">
-              Feeling Techy enough? Check out the{" "}
-              <Link href="/cli" className="italic underline">
-                CLI version
-              </Link>{" "}
-              of my portfolio.
-            </div>
+            <HomeCard />
           </div>
-        </Draggable>
+        </div>
       </div>
     </main>
   );
