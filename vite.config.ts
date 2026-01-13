36b58ed405105b6c2285791174b034c0a55f77bf
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2026-01-14 00:27:03 +0530

    Deploy stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -12,6 +13,7 @@ const config = defineConfig({
 		}),
 		tailwindcss(),
 		tanstackStart(),
+		nitro(),
 		viteReact(),
 	],
 	server: {
